761c108a939f6367b68a5878bbb8e09679fb74e6
Jai <vjai3388@gmail.com>
2025-09-13 12:14:46 +0530

    Initial Commit

---
+++ Task-1.py
@@ -0,0 +1,12 @@
+a=input("Enter the first number:")
+b=input("Enter the second number:")
+a=(int(a))
+b=(int(b))
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print('Addition: ',sum)
+print('Subtraction: ',sub)
+print('multiplication: ',mul)
+print('Division: ',div)
